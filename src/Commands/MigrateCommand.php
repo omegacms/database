bde1091ebb8b90430680d52d60e7d6ea71a41a17
omegacms <omegacms@outlook.com>
2024-01-17 20:51:46 +0100

    Adding MigraCommand

---
+++ src/Commands/MigrateCommand.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Part of Omega CMS - Commands Package
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
+
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @namespace
+ */
+namespace Omega\Database\Commands;
+
+/**
+ * @use
+ */
+use function getcwd;
+use function glob;
+use Omega\Database\Adapter\AbstractDatabaseAdapter;
+use Omega\Database\Adapter\MysqlAdapter;
+use Omega\Database\Adapter\SqliteAdapter;
+use Omega\Database\DatabaseFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Migrate command class.
+ *
+ * The `MigrateCommand` is used to run database migrations. It looks for migration
+ * files in the specified directory and executes them. You can also use the `--fresh`
+ * option to delete all database tables before running migrations.
+ *
+ * @category    Omega
+ * @package     Omega\Commands
+ * @link        https://omegacms.github.io
+ * @author      Adriano Giovannini <omegacms@outlook.com>
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version     1.0.0
+ */
+class MigrateCommand extends Command
+{
+    /**
+     * Default command name.
+     *
+     * @var string $defaultName Holds the default command name.
+     */
+    protected static $defaultName = 'migrate';
+
+    /**
+     * Configures the current command.
+     *
+     * This method configures the command description, options, and help information.
+     *
+     * @return void
+     */
+    protected function configure() : void
+    {
+        $this
+            ->setDescription( 'Migrates the database' )
+            ->addOption( 'fresh', null, InputOption::VALUE_NONE, 'Delete all tables before running the migrations' )
+            ->setHelp( 'This command looks for all migration files and runs them' );
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * This method runs database migrations by looking for migration files and executing
+     * them in order. It also provides an option to delete all database tables before
+     * running migrations.
+     *
+     * @param  InputInterface  $input  Holds an instance of InputInterface.
+     * @param  OutputInterface $output Holds an instance of OutputInterface.
+     * @return int Return 0 if everything went fine, or an exit code.
+     */
+    protected function execute( InputInterface $input, OutputInterface $output ) : int
+    {
+        $current = getcwd();
+        $pattern = 'database/migrations/*.php';
+
+        $paths = glob( "{$current}/{$pattern}" );
+
+        if ( count( $paths ) < 1 ) {
+            $this->writeln( 'No migrations found' );
+            return Command::SUCCESS;
+        }
+
+        $connection = $this->connection();
+
+        if ( $input->getOption( 'fresh' ) ) {
+            $output->writeln( 'Dropping existing database tables' );
+
+            $connection->dropTables();
+            $connection = $this->connection();
+        }
+
+        if ( ! $connection->hasTable( 'migrations' ) ) {
+            $output->writeln( 'Creating migrations table' );
+            $this->createMigrationsTable( $connection );
+        }
+
+        foreach ( $paths as $path ) {
+            [ $prefix, $file     ] = explode( '_', $path );
+            [ $class, $extension ] = explode( '.', $file );
+
+            require $path;
+
+            $output->writeln( "Migrating: {$class}" );
+
+            $obj = new $class();
+            $obj->migrate( $connection );
+
+            $connection
+                ->query()
+                ->from( 'migrations' )
+                ->insert( [ 'name' ], [ 'name' => $class ] );
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Connect to the database.
+     *
+     * This method initializes and connects to the database using the provided
+     * configuration.
+     *
+     * @return AbstractDatabaseAdapter Return an instance of AbstractDatabaseAdapter.
+     */
+    private function connection() : AbstractDatabaseAdapter
+    {
+        $factory = new DatabaseFactory();
+
+        $factory->register( 'mysql', function ( $config ) {
+            return new MysqlAdapter( $config );
+        } );
+
+        $factory->register( 'sqlite', function ( $config ) {
+            return new SqliteAdapter( $config );
+        } );
+
+        $config = require getcwd() . '/config/database.php';
+
+        return $factory->bootstrap( $config[ $config[ 'default' ] ] );
+    }
+
+    /**
+     * Create migration table.
+     *
+     * @param  AbstractDatabaseAdapter $connection Holds an instance of AbstractDatabaseAdapter.
+     * @return void
+     */
+    private function createMigrationsTable( AbstractDatabaseAdapter $connection ) : void
+    {
+        $table = $connection->createTable( 'migrations' );
+        $table->id( 'id' );
+        $table->string( 'name' );
+        $table->execute();
+    }
+}
