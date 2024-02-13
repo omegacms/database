80ed68ac5c5b99387044cf7a598ec2f544edaaf4
omegacms <omegacms@outlook.com>
2024-02-13 17:03:54 +0100

    Add QueryException class

---
+++ src/Exceptions/QueryException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Part of Omega CMS - Database Package
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
+
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @namespace
+ */
+namespace Omega\Database\Exceptions;
+
+/**
+ * @use
+ */
+use Exception;
+
+class QueryException extends Exception
+{
+
+}
