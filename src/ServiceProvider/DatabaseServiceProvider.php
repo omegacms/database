308ac8ac49727a6dd476bd3ba002f4da6bcc07c3
omegacms <omegacms@outlook.com>
2024-10-01 22:27:00 +0200

    Refactor service provider

--- src/ServiceProvider/DatabaseServiceProvider.php
+++ src/ServiceProvider/DatabaseServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Part of Omega CMS - Database Package
+ * Part of Omega CMS -  Database Package
  *
  * @link       https://omegacms.github.io
  * @author     Adriano Giovannini <omegacms@outlook.com>
@@ -21,65 +21,41 @@ namespace Omega\Database\ServiceProvider;
 /**
  * @use
  */
-use Closure;
-use Omega\Database\Adapter\MysqlAdapter;
-use Omega\Database\Adapter\SqliteAdapter;
-use Omega\Database\DatabaseFactory;
-use Omega\Container\ServiceProvider\AbstractServiceProvider;
+use Omega\Application\Application;
 use Omega\Container\ServiceProvider\ServiceProviderInterface;
+use Omega\Database\Factory\DatabaseFactory;
+use Omega\Support\Facades\Config;
 
 /**
  * Database service provider class.
  *
- * The `DatabaseServiceProvider` class managing the database connection. This service
- * provider is responsible for creating and managing instances of the DatabaseFactory
- * and providing drivers for different database types such as SQLite and MySQL.
+ * The `DatabaseServiceProvider` class is responsible for creating the DatabaseFactory instance
+ * and defining the available drivers for the session service, such as the 'native' driver.
  *
  * @category    Omega
- * @package     Omega\Database
- * @subpackage  Omega\Database\ServiceProvider
+ * @package     Database
+ * @subpackage  ServiceProvider
  * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
  * @copyright   Copyright (c) 2024 Adriano Giovannini. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
-class DatabaseServiceProvider extends AbstractServiceProvider
+class DatabaseServiceProvider implements ServiceProviderInterface
 {
     /**
      * @inheritdoc
-     *
-     * @return string Return the service name.
+     * 
+     * @param  Application $application Holds the main application container to which services are bound.
+     * @return void This method does not return a value.
      */
-    protected function name() : string
+    public function bind( Application $application ) : void // Non deve ritornare un factory
     {
-        return 'database';
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @return ServiceProviderInterface Return an instance of ServiceProviderIntrface.
-     */
-    protected function factory() : ServiceProviderInterface
-    {
-        return new DatabaseFactory();
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @return array<string, Closure> Return an array of drivers for the service.
-     */
-    protected function drivers() : array
-    {
-        return [
-            'sqlite' => function ( $config ) {
-                return new SqliteAdapter( $config );
-            },
-            'mysql'  => function ( $config ) {
-                return new MysqlAdapter( $config );
-            },
-        ];
+        $application->alias( 'database', function () {
+            $config  = Config::get( 'database' );
+            $default = $config[ 'default' ];
+            
+            return (new DatabaseFactory())->create($config[$default]);
+        });
     }
 }
